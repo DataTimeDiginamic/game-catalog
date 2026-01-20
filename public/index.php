b352539f9adb68ecb64730fcfc0ebf8f5ad76a4e
Vityy <ygensoftvd@gmail.com>
2026-01-20 11:10:11 +0100

    Added method to allow frontend connection

--- public/index.php
+++ public/index.php
@@ -1,5 +1,15 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
+
 use Controller\PingApiController;
 use Controller\ProjectController;
 use Controller\ClientController;
